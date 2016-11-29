2579689eadac7c9d636d905528583dddfeb9bb91
Scott Jackson <Scottyjackson97@gmail.com>
2016-11-29 00:25:40 -0600

    unpacking

--- src/main/scala/Collections.scala
+++ src/main/scala/Collections.scala
@@ -30,5 +30,42 @@ class RList(value: Type*) {
 
 // Utilities for the user to work with RVector
 object VectorUtils {
+	def unpackNumericVector(vec: RVector): List[Double] = {
+		if (vec.getType != "Numeric")
+			throw new IllegalArgumentException("Vector is not Numeric")
 
+		var retval = List[Double]()
+		for (v <- vec.data) {
+			v.storedValue match {
+				case d: Double => retval +:= d
+			}
+		}
+		return retval
+	} 
+
+	def unpackLogicalVector(vec: RVector): List[Boolean] = {
+		if (vec.getType != "Logical")
+			throw new IllegalArgumentException("Vector is not Logical")
+		
+		var retval = List[Boolean]()
+		for (v <- vec.data) {
+			v.storedValue match {
+				case b: Boolean => retval +:= b
+			}
+		}
+		return retval
+	}
+
+	def unpackCharacterVector(vec: RVector): List[String] = {
+		if (vec.getType != "Character")
+			throw new IllegalArgumentException("Vector is not Character")
+
+		var retval = List[String]()
+		for (v <- vec.data) {
+			v.storedValue match {
+				case s: String => retval +:= s
+			}
+		}
+		return retval
+	}
 }
